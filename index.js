b791a39a05cbaa95ac9b76479ebefa272f372a5d
loehur <loehur@gmail.com>
2024-05-22 11:59:05 +0700

    express22

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var s = new server({ port: PORT });
 
 const express = require('express')
 const app = express()
-const port = 443
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
